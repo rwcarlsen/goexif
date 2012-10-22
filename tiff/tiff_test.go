2a65b76b243b98633fc8fbe51d89302088604c0b
Robert Carlsen <rwcarlsen@gmail.com>
2012-10-22 14:05:16 -0500

    restructured table sets - consolidating big/little endian

--- tiff/tiff_test.go
+++ tiff/tiff_test.go
@@ -24,211 +24,141 @@ type output struct {
 }
 
 type tagTest struct {
-  in input
+  big input // big endian
+  little input // little endian
   out output
 }
 
 ///////////////////////////////////////////////
 //// Big endian Tests /////////////////////////
 ///////////////////////////////////////////////
-var bigEndSet = []tagTest{
+var set1 = []tagTest{
   //////////// string type //////////////
   tagTest{
     //   {"TgId", "TYPE", "N-VALUES", "OFFSET--", "VAL..."},
     input{"0003", "0002", "00000002", "11000000", ""},
-    output{0x0003, 0x0002, 0x0002, []byte{0x11, 0x00}},
-  },
-  tagTest{
-    input{"0001", "0002", "00000006", "00000012", "111213141516"},
-    output{0x0001, 0x0002, 0x0006, []byte{0x11, 0x12, 0x13, 0x14, 0x15, 0x16}},
-  },
-  //////////// int (1-byte) type ////////////////
-  tagTest{
-    input{"0001", "0001", "00000001", "11000000", ""},
-    output{0x0001, 0x0001, 0x0001, []byte{0x11}},
-  },
-  tagTest{
-    input{"0001", "0001", "00000005", "00000010", "1112131415"},
-    output{0x0001, 0x0001, 0x0005, []byte{0x11, 0x12, 0x13, 0x14, 0x15}},
-  },
-  tagTest{
-    input{"0001", "0006", "00000001", "11000000", ""},
-    output{0x0001, 0x0006, 0x0001, []byte{0x11}},
-  },
-  tagTest{
-    input{"0001", "0006", "00000005", "00000010", "1112131415"},
-    output{0x0001, 0x0006, 0x0005, []byte{0x11, 0x12, 0x13, 0x14, 0x15}},
-  },
-  //////////// int (2-byte) types ////////////////
-  tagTest{
-    input{"0001", "0003", "00000002", "11111212", ""},
-    output{0x0001, 0x0003, 0x0002, []byte{0x11, 0x11, 0x12, 0x12}},
-  },
-  tagTest{
-    input{"0001", "0003", "00000003", "00000010", "111213141516"},
-    output{0x0001, 0x0003, 0x0003, []byte{0x11, 0x12, 0x13, 0x14, 0x15, 0x16}},
-  },
-  tagTest{
-    input{"0001", "0008", "00000001", "11120000", ""},
-    output{0x0001, 0x0008, 0x0001, []byte{0x11, 0x12}},
-  },
-  tagTest{
-    input{"0001", "0008", "00000003", "00000100", "111213141516"},
-    output{0x0001, 0x0008, 0x0003, []byte{0x11, 0x12, 0x13, 0x14, 0x15, 0x16}},
-  },
-  //////////// int (4-byte) types ////////////////
-  tagTest{
-    input{"0001", "0004", "00000001", "11121314", ""},
-    output{0x0001, 0x0004, 0x0001, []byte{0x11, 0x12, 0x13, 0x14}},
-  },
-  tagTest{
-    input{"0001", "0004", "00000002", "00000010", "1112131415161718"},
-    output{0x0001, 0x0004, 0x0002, []byte{0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18}},
-  },
-  tagTest{
-    input{"0001", "0009", "00000001", "11121314", ""},
-    output{0x0001, 0x0009, 0x0001, []byte{0x11, 0x12, 0x13, 0x14}},
-  },
-  tagTest{
-    input{"0001", "0009", "00000002", "00000011", "1112131415161819"},
-    output{0x0001, 0x0009, 0x0002, []byte{0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x18, 0x19}},
-  },
-  //////////// rational types ////////////////////
-  tagTest{
-    input{"0001", "0005", "00000001", "00000010", "1112131415161718"},
-    output{0x0001, 0x0005, 0x0001, []byte{0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18}},
-  },
-  tagTest{
-    input{"0001", "000A", "00000001", "00000011", "1112131415161819"},
-    output{0x0001, 0x000A, 0x0001, []byte{0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x18, 0x19}},
-  },
-  //////////// float types ///////////////////////
-  tagTest{
-    input{"0001", "0005", "00000001", "00000010", "1112131415161718"},
-    output{0x0001, 0x0005, 0x0001, []byte{0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18}},
-  },
-  tagTest{
-    input{"0101", "000A", "00000001", "00000011", "1112131415161819"},
-    output{0x0101, 0x000A, 0x0001, []byte{0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x18, 0x19}},
-  },
-}
-
-///////////////////////////////////////////////
-//// Little endian Tests //////////////////////
-///////////////////////////////////////////////
-var littleEndSet = []tagTest{
-  //////////// string type //////////////
-  tagTest{
-    //   {"TgId", "TYPE", "N-VALUES", "OFFSET--", "VAL..."},
     input{"0300", "0200", "02000000", "11000000", ""},
     output{0x0003, 0x0002, 0x0002, []byte{0x11, 0x00}},
   },
   tagTest{
+    input{"0001", "0002", "00000006", "00000012", "111213141516"},
     input{"0100", "0200", "06000000", "12000000", "111213141516"},
     output{0x0001, 0x0002, 0x0006, []byte{0x11, 0x12, 0x13, 0x14, 0x15, 0x16}},
   },
   //////////// int (1-byte) type ////////////////
   tagTest{
+    input{"0001", "0001", "00000001", "11000000", ""},
     input{"0100", "0100", "01000000", "11000000", ""},
     output{0x0001, 0x0001, 0x0001, []byte{0x11}},
   },
   tagTest{
+    input{"0001", "0001", "00000005", "00000010", "1112131415"},
     input{"0100", "0100", "05000000", "10000000", "1112131415"},
     output{0x0001, 0x0001, 0x0005, []byte{0x11, 0x12, 0x13, 0x14, 0x15}},
   },
   tagTest{
+    input{"0001", "0006", "00000001", "11000000", ""},
     input{"0100", "0600", "01000000", "11000000", ""},
     output{0x0001, 0x0006, 0x0001, []byte{0x11}},
   },
   tagTest{
+    input{"0001", "0006", "00000005", "00000010", "1112131415"},
     input{"0100", "0600", "05000000", "10000000", "1112131415"},
     output{0x0001, 0x0006, 0x0005, []byte{0x11, 0x12, 0x13, 0x14, 0x15}},
   },
   //////////// int (2-byte) types ////////////////
   tagTest{
     input{"0001", "0003", "00000002", "11111212", ""},
+    input{"0100", "0300", "02000000", "11111212", ""},
     output{0x0001, 0x0003, 0x0002, []byte{0x11, 0x11, 0x12, 0x12}},
   },
   tagTest{
     input{"0001", "0003", "00000003", "00000010", "111213141516"},
+    input{"0100", "0300", "03000000", "10000000", "111213141516"},
     output{0x0001, 0x0003, 0x0003, []byte{0x11, 0x12, 0x13, 0x14, 0x15, 0x16}},
   },
   tagTest{
     input{"0001", "0008", "00000001", "11120000", ""},
+    input{"0100", "0800", "01000000", "11120000", ""},
     output{0x0001, 0x0008, 0x0001, []byte{0x11, 0x12}},
   },
   tagTest{
     input{"0001", "0008", "00000003", "00000100", "111213141516"},
+    input{"0100", "0800", "03000000", "00100000", "111213141516"},
     output{0x0001, 0x0008, 0x0003, []byte{0x11, 0x12, 0x13, 0x14, 0x15, 0x16}},
   },
   //////////// int (4-byte) types ////////////////
   tagTest{
     input{"0001", "0004", "00000001", "11121314", ""},
+    input{"0001", "0004", "00000001", "11121314", ""},
     output{0x0001, 0x0004, 0x0001, []byte{0x11, 0x12, 0x13, 0x14}},
   },
   tagTest{
+    input{"0001", "0004", "00000002", "00000010", "1112131415161718"},
     input{"0001", "0004", "00000002", "00000010", "1112131415161718"},
     output{0x0001, 0x0004, 0x0002, []byte{0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18}},
   },
   tagTest{
     input{"0001", "0009", "00000001", "11121314", ""},
+    input{"0001", "0009", "00000001", "11121314", ""},
     output{0x0001, 0x0009, 0x0001, []byte{0x11, 0x12, 0x13, 0x14}},
   },
   tagTest{
+    input{"0001", "0009", "00000002", "00000011", "1112131415161819"},
     input{"0001", "0009", "00000002", "00000011", "1112131415161819"},
     output{0x0001, 0x0009, 0x0002, []byte{0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x18, 0x19}},
   },
   //////////// rational types ////////////////////
   tagTest{
     input{"0001", "0005", "00000001", "00000010", "1112131415161718"},
+    input{"0001", "0005", "00000001", "00000010", "1112131415161718"},
     output{0x0001, 0x0005, 0x0001, []byte{0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18}},
   },
   tagTest{
+    input{"0001", "000A", "00000001", "00000011", "1112131415161819"},
     input{"0001", "000A", "00000001", "00000011", "1112131415161819"},
     output{0x0001, 0x000A, 0x0001, []byte{0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x18, 0x19}},
   },
   //////////// float types ///////////////////////
   tagTest{
     input{"0001", "0005", "00000001", "00000010", "1112131415161718"},
+    input{"0001", "0005", "00000001", "00000010", "1112131415161718"},
     output{0x0001, 0x0005, 0x0001, []byte{0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18}},
   },
   tagTest{
+    input{"0101", "000A", "00000001", "00000011", "1112131415161819"},
     input{"0101", "000A", "00000001", "00000011", "1112131415161819"},
     output{0x0101, 0x000A, 0x0001, []byte{0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x18, 0x19}},
   },
 }
 
-func TestDecodeTag_bigendian(t *testing.T) {
-  testSet(t, binary.BigEndian, bigEndSet)
+func TestDecodeTag(t *testing.T) {
+  for i, tst := range set1 {
+    testSingle(t, binary.BigEndian, tst.big, tst.out, i)
+    testSingle(t, binary.LittleEndian, tst.little, tst.out, i)
+  }
 }
 
-func TestDecodeTag_littleendian(t *testing.T) {
-  testSet(t, binary.LittleEndian, littleEndSet)
-}
+func testSingle(t *testing.T, order binary.ByteOrder, in input, out output, i int) {
+  data := buildInput(in, order)
+  buf := bytes.NewReader(data)
+  tg, err := DecodeTag(buf, order)
+  if err != nil {
+    t.Errorf("(%v) tag %v%+v decode failed: %v", order, i, in, err)
+    return
+  }
 
-func testSet(t *testing.T, order binary.ByteOrder, set []tagTest) {
-  for i, tst := range set {
-    data := buildInput(tst.in, order)
-    buf := bytes.NewReader(data)
-
-    tg, err := DecodeTag(buf, order)
-    if err != nil {
-      t.Errorf("tag %v%+v decode failed: %v", i, tst.in, err)
-      continue
-    }
-
-    if tg.Id != tst.out.id {
-      t.Errorf("tag %v id decode: expected %v, got %v", i, tst.out.id, tg.Id)
-    }
-    if tg.Fmt != tst.out.format {
-      t.Errorf("tag %v format decode: expected %v, got %v", i, tst.out.format, tg.Fmt)
-    }
-    if tg.Ncomp != tst.out.count {
-      t.Errorf("tag %v N-components decode: expected %v, got %v", i, tst.out.count, tg.Ncomp)
-    }
-    if ! bytes.Equal(tg.Val, tst.out.val) {
-      t.Errorf("tag %v value decode: expected %v, got %v", i, tst.out.val, tg.Val)
-    }
+  if tg.Id != out.id {
+    t.Errorf("(%v) tag %v id decode: expected %v, got %v", order, i, out.id, tg.Id)
+  }
+  if tg.Fmt != out.format {
+    t.Errorf("(%v) tag %v format decode: expected %v, got %v", order, i, out.format, tg.Fmt)
+  }
+  if tg.Ncomp != out.count {
+    t.Errorf("(%v) tag %v N-components decode: expected %v, got %v", order, i, out.count, tg.Ncomp)
+  }
+  if ! bytes.Equal(tg.Val, out.val) {
+    t.Errorf("(%v) tag %v value decode: expected %v, got %v", order, i, out.val, tg.Val)
   }
 }
 
