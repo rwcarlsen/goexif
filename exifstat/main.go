04c10506c3acbb7fcbb0dd395097ed3f9ad9c2d5
Robert Carlsen <rwcarlsen@gmail.com>
2012-12-11 22:48:57 -0600

    exifstat works on multiple files

--- exifstat/main.go
+++ exifstat/main.go
@@ -6,31 +6,37 @@ import (
 	"fmt"
 	"log"
 	"flag"
+
 	"github.com/rwcarlsen/goexif/exif"
 	"github.com/rwcarlsen/goexif/tiff"
 )
 
 func main() {
 	flag.Parse()
-	fname := flag.Arg(0)
+	fnames := flag.Args()
 
-	f, err := os.Open(fname)
-	if err != nil {
-		log.Fatal(err)
+	for _, name := range fnames {
+		f, err := os.Open(name)
+		if err != nil {
+			log.Printf("err on %v: %v", name, err)
+			continue
+		}
+
+		x, err := exif.Decode(f)
+		if err != nil {
+			log.Printf("err on %v: %v", name, err)
+			continue
+		}
+
+		fmt.Printf("\n---- Image '%v' ----\n", name)
+		x.Walk(Walker{})
 	}
-
-	x, err := exif.Decode(f)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	x.Walk(Walker{})
 }
 
 type Walker struct {}
 
 func (_ Walker) Walk(name exif.FieldName, tag *tiff.Tag) error {
 	data, _ := tag.MarshalJSON()
-	fmt.Printf("%v: %v\n", name, string(data))
+	fmt.Printf("    %v: %v\n", name, string(data))
 	return nil
 }
